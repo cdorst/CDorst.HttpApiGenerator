234b95ea4ec29e76539345e6457d2dfa3a7ed2bf
Christopher Dorst <chris@venassure.com>
2021-11-14 23:27:22 -0600

    Use one type per file

---
+++ CDorst.HttpApiGenerator/ProcedureParameter.cs
@@ -0,0 +1,5 @@
+﻿using System.Collections.Generic;
+
+namespace CDorst.HttpApiGenerator;
+
+internal record struct ProcedureParameter(string Name, string Type, int? StringLength = default, bool Required = false, bool Positive = false, string? DefaultValue = default, IEnumerable<ProcedureParameter>? TableParameters = default);
---
+++ CDorst.HttpApiGenerator/StringExtensions.cs
@@ -0,0 +1,50 @@
+﻿namespace CDorst.HttpApiGenerator;
+
+internal static class StringExtensions
+{
+    public static string ToSqlDataReaderType(this string type)
+        => type switch
+        {
+            "string?" => "String",
+            "char?" => "Char",
+            "bool?" => "Boolean",
+            "int?" => "Int32",
+            "short?" => "Int16",
+            "long?" => "Int64",
+            "DateTime?" => "DateTime",
+            "DateTimeOffset?" => "DateTimeOffset",
+            "decimal?" => "Decimal",
+            "double?" => "Double",
+            "Guid?" => "Guid",
+            _ => type,
+        };
+
+    public static string ToSystemType(this string type)
+    {
+        var typeUpper = type.ToUpper();
+        if (typeUpper.Contains("VARCHAR")) return "string";
+        return typeUpper switch
+        {
+            "DATETIMEOFFSET" => "DateTimeOffset",
+            "DATETIME" => "DateTime",
+            "DATE" => "DateTime",
+            "BIGINT" => "long",
+            "INT" => "int",
+            "SMALLINT" => "short",
+            "SHORT" => "short",
+            "TINYINT" => "byte",
+            "BIT" => "byte",
+            "BYTE" => "byte",
+            "DECIMAL" => "decimal",
+            "DOUBLE" => "double",
+            "GUID" => "Guid",
+            "CHAR" => "char",
+            "BOOL" => "bool",
+            "BOOLEAN" => "bool",
+            _ => type,
+        };
+    }
+
+    public static string ToSystemTypeNullable(this string type)
+        => type.ToSystemType() + '?';
+}
